2ec1cb365717149d6cc1730a33c3ac5f58bf4a3e
Mystery <31568969+mysterymm@users.noreply.github.com>
2018-10-28 05:32:48 -0700

    Disabler
     The batch Tool

---
+++ Disabler.bat
@@ -0,0 +1,35 @@
+@echo off
+title tool for gamers
+
+color 4
+cls
+
+
+
+echo                                     "==================="
+echo                                     "| Tool For Gamers |" 
+echo                                     "=================== "
+echo                                     "| Coded By Mystery| "
+echo                                      "\ ||||||||||||||/ "
+echo                                      " --------------- "
+echo                                "to make sure your performance 100%"
+echo                                "Follow me if you appreciate this tool"
+echo                                "IG: SadMystery Discord: Depressive#0666"
+
+timeout 5
+cls
+
+net.exe stop "Windows search"
+timeout 4
+cls
+
+net.exe stop superfetch
+timeout 6
+cls
+
+
+echo     "preformance enhanced :)"
+
+
+
+
